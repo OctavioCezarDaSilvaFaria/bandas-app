672cad9d8c27b6aa25da64fef9e66873a119b4ff
OctavioCezarDaSilvaFaria <tavinhojabuca@gmail.com>
2024-04-04 09:15:06 -0300

    Segunda Modificação do Spotify do Paraguai

---
+++ src/screens/MusicDetails.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function MusicDetails() {
+    return (
+        <View>
+            
+            <Text>Detalhe das Músicas</Text>
+        
+        </View>      
+    );
+}
